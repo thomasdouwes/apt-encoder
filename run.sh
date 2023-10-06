564bc5dbcdbd76948cac85fd9cf85bbc8ba23380
git <thomas@douwes.co.uk>
2023-10-06 16:44:56 +0100

    fix run script

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ chb=$2
 convert $cha -resize 909x -colorspace gray -depth 8 -compress none cha.pgm
 convert $chb -resize 909x -colorspace gray -depth 8 -compress none chb.pgm
 
-./apt-encode cha.pgm chab.pgm | sox -t raw -b 8 -e unsigned -c 1 -r 8320 - -r 8320 apt.wav
+./apt-encode cha.pgm chb.pgm | sox -t raw -b 8 -e unsigned -c 1 -r 12480 - -r 12480 apt.wav
+
+noaa-apt apt.wav
